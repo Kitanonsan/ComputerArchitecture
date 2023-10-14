e759bd1eccaac9d7c3c4677fac15a9406992e937
Relpantagon <pichayoothunchainao@gmail.com>
2023-10-14 17:37:07 +0700

    Simulator Comment Complete

--- src/main/java/Simulator/Simulator.java
+++ src/main/java/Simulator/Simulator.java
@@ -5,10 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Simulator {
-    private static final int NUMMEMORY = 65536; // maximum number of words in memory
-    private static final int NUMREGS = 8; // number of machine registers
-    private static final int MAXLINELENGTH = 1000;
-    private static final String filename = "src/IOFile/Machine-code.txt"; //Change this to your file's name
+    private static final int NUMMEMORY = 65536; //Maximum number of words in memory
+    private static final int NUMREGS = 8; //number of machine registers
+    private static final int MAXLINELENGTH = 1000; //Max Line of program
+    private static final String filename = "src/IOFile/Machine-code.txt"; //File receive from Assembler (Machine-Code)
 
     static class State {
         int pc;
@@ -25,8 +25,10 @@ public class Simulator {
     }
 
     public Simulator() {
+        //Initialize the simulator state
         State state = new State();
 
+        //Read machine code instructions from the file
         try (BufferedReader fileReader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = fileReader.readLine()) != null) {
@@ -35,20 +37,19 @@ public class Simulator {
                     System.exit(1);
                 }
 
-                // Parse as a decimal integer
+                //Parse as a decimal integer
                 int decimalValue = Integer.parseInt(line);
 
-                // Add the binaryValue to memory
+                //Add the binaryValue to memory
                 state.mem[state.numMemory] = decimalValue;
                 state.numMemory++;
 
-                // Print the memory value
+                //Print the memory value
                 System.out.println("memory[" + (state.numMemory - 1) + "]=" + decimalValue);
             }
 
-
-            // Execute logic based on opcode (you can call Execute.executeInstruction here)
         } catch (IOException e) {
+            //Handle file not found
             System.err.println("error: can't find file " + filename);
             e.printStackTrace();
             System.exit(1);
@@ -57,11 +58,10 @@ public class Simulator {
 
         int sum=0;
         while (true) {
-
-            // Print the current state before executing each instruction
+            //Print the current state before executing each instruction
             printState(state);
 
-            // Fetch the current instruction from memory
+            //Fetch the current instruction from memory
             int intValue = state.mem[state.pc];
             String binaryValue = toTwosComplementBinary(intValue);
             int opcode = extractOpcode(binaryValue);
@@ -69,112 +69,112 @@ public class Simulator {
             int regA,regB,destReg,offsetField;
             int memoryAddress;
             switch (opcodeBinary) {
-                case "000" -> { // add
-
-                    //string cutting
+                case "000" -> { //add operation
+                    //Seperate register and destination position
                     regA = Integer.parseInt(binaryValue.substring(10, 13), 2);
                     regB = Integer.parseInt(binaryValue.substring(13, 16), 2);
                     destReg = Integer.parseInt(binaryValue.substring(29, 32), 2);
 
-                    // Execute add instruction logic
+                    //Execute
                     state.reg[destReg] = state.reg[regA] + state.reg[regB];
+
+                    //program counter Increase
                     state.pc++;
                     sum++;
                 }
-                case "001" -> { // nand
-                    //string cutting
+                case "001" -> { //nand operation
+                    // Separate register and destination position
                     regA = Integer.parseInt(binaryValue.substring(10, 13), 2);
                     regB = Integer.parseInt(binaryValue.substring(13, 16), 2);
                     destReg = Integer.parseInt(binaryValue.substring(29, 32), 2);
 
-                    // Execute nand instruction logic
+                    //Execute
                     state.reg[destReg] = ~(state.reg[regA] & state.reg[regB]);
-                    // Increment the program counter
+                    //program counter Increase
                     state.pc++;
                     sum++;
                 }
-                case "010" -> { // lw
-                    // string cutting
+                case "010" -> { //load operation
+                    //Separate register and offset field position
                     regA = Integer.parseInt(binaryValue.substring(10, 13), 2);
                     regB = Integer.parseInt(binaryValue.substring(13, 16), 2);
                     offsetField = Integer.parseInt(binaryValue.substring(16, 32), 2);
                     offsetField = convertNum(offsetField);
 
-                    // Calculate the memory address by adding offsetField to regA
+                    //Calculate memory address(adding offsetField to regA)
                     memoryAddress = state.reg[regA] + offsetField;
-                    // Check if memoryAddress is within bounds
+                    //Check if memoryAddress is within bounds
                     if (memoryAddress < 0 || memoryAddress >= NUMMEMORY) {
                         System.err.println("ERROR: Invalid memory address");
                         System.exit(1);
                     }
 
-                    // Load the value from memory into regB
+                    //Load value from memory to regB
                     state.reg[regB] = state.mem[memoryAddress];
 
-                    // Increment the program counter
+                    //program counter Increase
                     state.pc++;
                     sum++;
                 }
-                case "011" -> { // sw
-                    //string cutting
+                case "011" -> { // store operation
+                    //Separate register and offset field position
                     regA = Integer.parseInt(binaryValue.substring(10, 13), 2);
                     regB = Integer.parseInt(binaryValue.substring(13, 16), 2);
                     offsetField = Integer.parseInt(binaryValue.substring(16, 32), 2);
                     offsetField = convertNum(offsetField);
 
-                    // Calculate the memory address by adding offsetField to regA
+                    //Calculate memory address(adding offsetField to regA)
                     memoryAddress = state.reg[regA] + offsetField;
-                    // Check if memoryAddress is within bounds
+                    //Check if memoryAddress is within bounds
                     if (memoryAddress < 0 || memoryAddress >= NUMMEMORY) {
                         System.err.println("ERROR: Invalid memory address");
                         System.exit(1);
                     }
 
-                    // Store the value from regB into memory at the calculated address
+                    //Store value from regBto memory at destination address
                     state.mem[memoryAddress] = state.reg[regB];
-                    // Increment the program counter
+                    //program counter Increase
                     state.pc++;
                     sum++;
                 }
-                case "100" -> { // beq
-                    // string cutting
+                case "100" -> { //branch equal operation
+                    //Separate register and offset field position
                     regA = Integer.parseInt(binaryValue.substring(10, 13), 2);
                     regB = Integer.parseInt(binaryValue.substring(13, 16), 2);
                     offsetField = Integer.parseInt(binaryValue.substring(16, 32), 2);
                     offsetField = convertNum(offsetField);
 
 
-                    // Check if regA and regB are equal
+                    // Check regA and regB is equal?
                     if (state.reg[regA] == state.reg[regB]) {
-                        // Jump to the address PC + 1 + offsetField
+                        //Jump to the address PC + 1 + offsetField
                         sum++;
                         state.pc += (1 + offsetField);
                     } else {
-                        // Increment the program counter
-                        sum++;
-                        // Increment the program counter
+                        //program counter Increase
                         state.pc++;
+                        sum++;
                     }
                 }
-                case "101" -> { // jalr
-                    // string cutting
+                case "101" -> { // jump and link return
+                    //Separate register  position
                     regA = Integer.parseInt(binaryValue.substring(10, 13), 2);
                     regB = Integer.parseInt(binaryValue.substring(13, 16), 2);
 
-                    // Store the value PC + 1 in regB
+                    //Store value PC + 1 to regB
                     state.reg[regB] = state.pc + 1;
 
+                    //If regA and regB equal, store PC + 1 in regB
                     if (regA == regB) {
-                        // If regA and regB are the same, store PC + 1 in regB first
                         state.pc++;
                     } else {
-                        // Jump to the address stored in regA
+                        //Jump to address that stored in regA
                         state.pc = state.reg[regA];
                     }
                     sum++;
                 }
-                case "110" -> { // halt
-                    // string cutting
+                case "110" -> { // halt end of program
+                    //stop program
                     sum++;
                     System.out.println("machine halted");
                     System.out.println("total of " + sum + " instructions executed");
@@ -185,12 +185,12 @@ public class Simulator {
                     return;
                 }
                 case "111" -> { // noop
-                    // Do nothing
+                    //Do nothing
                     sum++;
                 }
 
                 default -> {
-                    // Handle unknown opcode
+                    //Handle unknown opcode
                     System.err.println("ERROR: Memory at location " + state.pc + " illegible.");
                     System.exit(1);
                 }
@@ -199,39 +199,42 @@ public class Simulator {
         }
     }
 
-
-    // Define the convertNum function
+    //Define the convertNum function
     static int convertNum(int num) {
+        //Change 16bit 2's complement number to signed integer
         if ((num & (1 << 15)) != 0) {
             num -= (1 << 16);
         }
         return num;
     }
-
+    //Separate opcode out of binary instruction
     static int extractOpcode(String binaryValue) {
-        // Shift right 22 bits and perform a bitwise AND with 0b111 to extract opcode.
+        // Shift right 22 bit then AND with 0b111 to get opcode
         return (Integer.parseInt(binaryValue, 2) >> 22) & 0b111;
     }
 
+    //Change integer value to a 32bit 2's complement binary
     static String toTwosComplementBinary(int value) {
         StringBuilder binary = new StringBuilder(Integer.toBinaryString(value));
-        //System.out.println("in 2's complement " + binary);
         int length = binary.length();
 
-        // Pad with leading 0s to get a fixed-length representation
+        //Make sure that binary is in 32bit long by adding 0
         while (length < 32) {
             binary.insert(0, "0");
             length++;
         }
 
-        // If the original value was positive, return the binary representation as is
+        //If value is positive, return the 2's complement binary
         if (value >= 0) {
             return binary.toString();
         }
+        //If value is negative, return the 2's complement binary format
+
         return binary.toString();
 
     }
 
+    //Print state of the simulator
     static void printState(State state) {
         System.out.println("\n@@@\nstate:");
         System.out.println("\tpc " + state.pc);
