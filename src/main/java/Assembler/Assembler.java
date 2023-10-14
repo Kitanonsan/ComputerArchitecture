e8350df814f2fe48d8ee6ea9d00b5a2f3648f722
Kitanonsan <Kitanon_san@cmu.ac.th>
2023-10-14 16:33:13 +0700

    Add Fast multiplication program

--- src/main/java/Assembler/Assembler.java
+++ src/main/java/Assembler/Assembler.java
@@ -40,17 +40,14 @@ public class Assembler {
         machine_code = new ArrayList<>();
         LabelMapping();
         MachineCode();
+        printBinaryMachineCode();
+        printDecimalMachineCode();
         try {
             File fileout = new File("src/IOFile/output.txt");
             FileOutputStream fos = new FileOutputStream(fileout);
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
             for (int i = 0; i < machine_code.size(); i++) {
-                String Decimal = "";
-                if (machine_code.get(i).length() == 32) {
-                    Decimal = String.valueOf(Integer.parseInt(machine_code.get(i), 2));
-                } else {
-                    Decimal = String.valueOf((short) Integer.parseInt(machine_code.get(i), 2));
-                }
+                String Decimal = String.valueOf(decimalValueOf(machine_code.get(i)));
                 writer.write(Decimal);
                 writer.newLine();
             }
@@ -189,7 +186,7 @@ public class Assembler {
                 if (number.matches(Format.Label)) {
                     number = LabelLine.get(number).toString();
                 }
-                binary.append(twoComplement(number));
+                binary.append(fillTwoComplement(number));
             }
             machine_code.add(binary.toString());
         }
@@ -220,6 +217,29 @@ public class Assembler {
         } else {
             throw new OffsetOutofRange(number + " : The number must between -32768 to 32767");
         }
+    }
+
+    public static String fillTwoComplement(String number){
+        int n = Integer.parseInt(number);
+        String twoComplement;
+        StringBuilder str;
+        String result;
+        if(n < 0){
+            n = -1*n;
+            twoComplement = Integer.toBinaryString(((~n) + 1));
+            str = new StringBuilder(twoComplement);
+            result = str.toString();
+        }
+        else{
+            twoComplement = Integer.toBinaryString(n);
+            str = new StringBuilder(twoComplement);
+            StringBuilder extend = new StringBuilder();
+            while (str.length() + extend.length() < 32) {
+                extend.append("0");
+            }
+            result = extend.append(str).toString();
+        }
+        return result;
     }
 
     //require: number of register
@@ -266,13 +286,18 @@ public class Assembler {
     //print machine code in decimal
     private void printDecimalMachineCode() {
         for (int i = 0; i < machine_code.size(); i++) {
-            if (machine_code.get(i).length() == 32) {
-                System.out.println(Integer.parseInt(machine_code.get(i), 2));
-            } else {
-                short labelValue = (short) Integer.parseInt(machine_code.get(i), 2);
-                System.out.println(labelValue);
-            }
-
+            System.out.println(decimalValueOf(machine_code.get(i)));
         }
+    }
+
+    public static int decimalValueOf(String number){
+        double result = 0;
+        if(number.charAt(0) == '1'){
+            result = result -Math.pow(2,31) + Long.parseUnsignedLong(number.substring(1,number.length()),2);
+        }
+        else{
+            result = Integer.parseInt(number,2);
+        }
+        return (int) result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import Assembler.Assembler;
 import Simulator.Simulator;
+import java.math.*;
 
 public class Main {
     public static void main(String[] args) {
         new Assembler();
         new Simulator();
     }
+
 }
